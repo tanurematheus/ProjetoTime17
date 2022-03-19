284b879011e6b8f7430fd61e412fcabc3ff351ca
tanurematheus <tanure.matheus@gmail.com>
2022-03-19 11:44:18 -0300

    add top-bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <v-app>
+    <AppTopBar />
     <router-view />
   </v-app>
 </template>
 
+<script>
+import AppTopBar from "./components/shared/AppTopBar";
+
+export default {
+  name: "App",
+  components: {
+    AppTopBar,
+  },
+};
+</script>
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app-bar color="indigo" hide-on-scroll app>
+    <v-toolbar-title>
+      <h3>
+        <strong> E-Páscoa </strong>
+      </h3>
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn
+      v-on="on"
+      v-bind="attrs"
+      color="transparent"
+      small
+      fab
+      @click="darkMode"
+    >
+      <div v-if="!$vuetify.theme.dark">
+        <v-icon>mdi-egg</v-icon>
+      </div>
+
+      <div v-else>
+        <v-icon>mdi-egg-outline</v-icon>
+      </div>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppTopBar",
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  methods: {
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
+
