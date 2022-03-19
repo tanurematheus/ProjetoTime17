be0c442d29df5f72b2110fdcc097468dd5636612
Ricardo <ricardomr8@hotmail.com>
2022-03-19 16:25:22 -0300

    ajuste time

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card
-    class="mx-auto"
+    class="ma-10"
     max-width="344"
     outlined>
     <v-list-item three-line>
@@ -10,21 +10,16 @@
         </v-list-item-title>
         <v-list-item-subtitle>Desenvolvedora</v-list-item-subtitle>
       </v-list-item-content>
-      <v-list-item-avatar
-        tile
-        color="primary"
-        size="128"></v-avatar>
-      ></v-list-item-avatar>
     </v-list-item>
 
       <v-card-actions>
-      <v-btn a href="https://www.linkedin.com/in/camilla-naomy-tsuda-33839b133/"></a>
+      <v-btn a href="https://www.linkedin.com/in/camilla-naomy-tsuda-33839b133/"
         outlined
         rounded
         text>
         Linkdelin
       </v-btn>
-      <v-btn a href="https://github.com/naomy19"></a>
+      <v-btn a href="https://github.com/naomy19"
         outlined
         rounded
         text>
@@ -33,146 +28,8 @@
     </v-card-actions>
   </v-card>
 
-  <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="text-h5 mb-1">
-              Gustavo Luiz Quadra
-            </v-list-item-title>
-            <v-list-item-subtitle>Desenvolvedor</v-list-item-subtitle>
-          </v-list-item-content>
 
-          <v-list-item-avatar
-          tile
-          color="primary"
-          size="128"></v-avatar>
-          </v-list-item>
 
-        <v-card-actions>
-          <v-btn a href="https://www.linkedin.com/in/gustavoquadra/"></a>
-        outlined
-        rounded
-        text>
-        Linkdelin
-      </v-btn>
-      <v-btn a href="https://github.com/gustavolq"></a>
-        outlined
-        rounded
-        text>
-        GitHub
-        </v-card-actions>
-  </v-card>
-
-  <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="text-h5 mb-1">
-              Leidirene Marques
-            </v-list-item-title>
-            <v-list-item-subtitle>Desenvolvedora</v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-avatar
-            tile
-            color="primary"
-            size="128"></v-avatar>
-          </v-list-item>
-
-        <v-card-actions>
-          <v-btn a href="https://www.linkedin.com/in/leidirene/"></a>
-        outlined
-        rounded
-        text>
-        Linkdelin
-      </v-btn>
-      <v-btn a href="https://github.com/Leidirene"></a>
-        outlined
-        rounded
-        text>
-        GitHub
-        </v-card-actions>
-  </v-card>
-
-  <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="text-h5 mb-1">
-              Matheus Tanure
-            </v-list-item-title>
-            <v-list-item-subtitle>Desenvolvedora</v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-avatar color="primary" size="120"></v-list-item-avatar>
-        </v-list-item>
-
-        <v-card-actions>
-          <v-btn a href="https://www.linkedin.com/in/matheus-tanure/"></a>
-        outlined
-        rounded
-        text>
-        Linkdelin
-      </v-btn>
-      <v-btn a href="https://github.com/tanurematheus"></a>
-        outlined
-        rounded
-        text>
-        GitHub
-        </v-card-actions>
-  </v-card>
-
-  <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="text-h5 mb-1">
-             Ricardo Muraoka
-            </v-list-item-title>
-            <v-list-item-subtitle>Desenvolvedora</v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-avatar color="primary" size="120"></v-list-item-avatar>
-        </v-list-item>
-
-        <v-card-actions>
-          <v-btn a href="https://www.linkedin.com/in/ricardo-muraoka/"></a>
-        outlined
-        rounded
-        text>
-        Linkdelin
-      </v-btn>
-      <v-btn a href="https://github.com/ricardomuraoka"></a>
-        outlined
-        rounded
-        text>
-        GitHub
-        </v-card-actions>
-  </v-card>
-
-  <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="text-h5 mb-1">
-              Vitor Hugo Gonçalves Costa
-            </v-list-item-title>
-            <v-list-item-subtitle>Desenvolvedor</v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-avatar color="primary" size="120"></v-list-item-avatar>
-        </v-list-item>
-
-        <v-card-actions>
-          <v-btn a href="https://www.linkedin.com/in/vhgc1/"></a>
-        outlined
-        rounded
-        text>
-        Linkdelin
-      </v-btn>
-      <v-btn a href="https://github.com/VHGC1"></a>
-        outlined
-        rounded
-        text>
-        GitHub
-        </v-card-actions>
-  </v-card>
 </template>
 
 <script>
