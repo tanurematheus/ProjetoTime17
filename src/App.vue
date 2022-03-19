31a9ccd177f0ac62361623ba3e0580750ace25da
tanurematheus <tanure.matheus@gmail.com>
2022-03-19 12:13:39 -0300

    add Bottom Bar to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <v-app>
     <AppTopBar />
     <router-view />
+    <BottomBar />
   </v-app>
 </template>
 
 <script>
 import AppTopBar from "./components/shared/AppTopBar";
+import BottomBar from "./components/shared/BottomBar.vue";
 
 export default {
   name: "App",
   components: {
     AppTopBar,
+    BottomBar,
   },
 };
 </script>
