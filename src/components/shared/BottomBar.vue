24c32ee69cfc420ea0af17517f3838b3bbbb1b68
Ricardo <ricardomr8@hotmail.com>
2022-03-19 12:07:48 -0300

    BottomBar alterado

--- src/components/shared/BottomBar.vue
+++ src/components/shared/BottomBar.vue
@@ -1,30 +1,43 @@
 <template>
   <div class="mt-10">
-    <v-bottom-navigation app :value="value" color="grey" dark >
-      <router-link  class="text-decoration-none" to="/">
+    <v-bottom-navigation
+      background-color="indigo"
+      active-class="green--text text--lighten-2"
+      app
+      :value="value"
+      color="grey"
+      dark
+    >
+      <router-link class="text-decoration-none" to="/">
         <v-btn retain-focus-on-click class="mt-2">
-        <span>Home</span>
+          <span>Home</span>
 
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
       </router-link>
 
       <router-link class="text-decoration-none" to="/lojas">
-        <v-btn 
-        class="mt-2"
-        >
+        <v-btn class="mt-2">
           <span>Lojas</span>
 
           <v-icon>mdi-store-search-outline</v-icon>
         </v-btn>
       </router-link>
 
-      <router-link class="text-decoration-none" to="/about">
+      <router-link class="text-decoration-none" to="/sobre">
         <v-btn class="mt-2">
-        <span>Produtos</span>
+          <span>Time</span>
 
-        <v-icon>mdi-egg-easter</v-icon>
-      </v-btn>
+          <v-icon>mdi-microsoft-teams</v-icon>
+        </v-btn>
+      </router-link>
+
+      <router-link class="text-decoration-none" to="/sobre">
+        <v-btn class="mt-2">
+          <span>Login</span>
+
+          <v-icon>mdi-login</v-icon>
+        </v-btn>
       </router-link>
     </v-bottom-navigation>
   </div>
