4d92411a31b394f93b0981eed543a9fe537c7540
Gustavo Luiz Quadra <gglquadra@gmail.com>
2022-03-19 13:16:57 -0300

    Update HomeView.vue, Add assets/Home-img.png

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
   <v-container>
-    <h1 class="text-center">Pagina Inicial</h1>
+    <div class = "mt-7 home-img">
+      <img src="../assets/Home-img.png" alt="Coleta">
+    </div>
+    <p class="text-home text-justify"> Bem-vindo </p>
   </v-container>
 </template>
 
+<style scoped>
+.home-img {
+    display: flex;
+    justify-content: center;
+    height:100%;
+  }
+
+.text-home {
+    font-size: 14px;
+  }
+</style>
